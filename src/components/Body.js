0973c5ee37dedf080caed2de8f4905c71bfe2b7f
Guilherme Abel <guilherme.silva@hiplatform.com>
2022-10-06 01:22:48 -0300

    added colums: id, name, artists, duration, image

--- src/components/Body.js
+++ src/components/Body.js
@@ -57,6 +57,74 @@ export default function Body() {
               <p className="description">{selectedPlaylist.description}</p>
             </div>
           </div>
+          <div className="list">
+            <div className="header_row">
+               <div className="col">
+                <span> 
+                  #
+                </span>
+                </div>
+                <div className="col">
+                <span> 
+                  TITLE
+                </span>
+                </div>
+                <div className="col">
+                <span> 
+                  ALBUM
+                </span>
+                </div>
+                <div className="col">
+                <span> 
+                <AiFillClockCircle />
+                </span>
+                </div>
+            </div>
+            <div className="tracks">
+              {
+                selectedPlaylist.tracks
+                .map(({
+                    id,
+                    name,
+                    artists,
+                    image,
+                    duration,
+                    album,
+                    context_uri,
+                    track_number
+                  },index) => {
+                    return (
+                      <div className="row" key={id}>
+                        <div className="col">
+                          <span>
+                            {index+1}
+                          </span>
+                          </div>
+                          <div className="col detail">
+                            <div className="img">
+                            <img src={image} alt="track" />
+                            </div>
+                          <div className="info">
+                            <span className="name">
+                              {name }
+                            </span>
+                            <span>
+                              {artists}
+                            </span>
+                          </div>
+                          </div>
+                          <div className="col">
+                            <span>{album}</span>
+                          </div>
+                          <div className="col">
+                            <span>{duration}</span>
+                          </div>
+                      </div>
+                    )
+                  })}
+
+            </div>
+          </div>
         </>
       )}
     </Container>
