4a17287277f717c4cf1d74ae26124daaeb2a5885
Alan Gabriel <contato@alangabriel.dev>
2022-10-24 16:08:34 -0300

    feat: add type to search

--- src/services/search.js
+++ src/services/search.js
@@ -1,8 +1,8 @@
 import qs from 'query-string'
 import { api } from './api'
 
-export const search = async (term) => {
-  const query = qs.stringify({ q: encodeURIComponent(term), type: 'artist,album,track' })
+export const search = async (term, type = ['artist', 'album', 'track']) => {
+  const query = qs.stringify({ q: encodeURIComponent(term), type: type.join() })
   try {
     const response = await api.get(`/search?${query}`)
     return response.data
