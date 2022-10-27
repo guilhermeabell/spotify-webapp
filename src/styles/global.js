5a206e96782160844cf8d9803882509ddb2207e7
Guilherme Abel <guilherme.silva@hiplatform.com>
2022-10-27 10:55:39 -0300

    added

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,6 +5,6 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
     padding: 0;
     margin: 0;
-    font-family: sans-serif;
+    font-family: Arial, Helvetica, sans-serif;
   }
 `
