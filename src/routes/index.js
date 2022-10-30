783a66fa57703afba1e6b3ac9054caa00abc6c19
Guilherme Abel <guilherme.silva@hiplatform.com>
2022-10-30 16:41:07 -0300

    added: lazy imports Code splitting in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
-import Auth from '../pages/Auth'
-import Home from '../pages/Home'
-import Login from '../pages/Login'
 import ProtectedRoute from './ProtectedRoute'
 import VolumeRange from '../components/VolumeRange'
+import Loader from '../components/Loader'
+
+const Login = lazy(() => import('../pages/Login'))
+const Home = lazy(() => import('../pages/Home'))
+const Auth = lazy(() => import('../pages/Auth'))
 
 const AllRoutes = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/volume" element={<VolumeRange />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/volume" element={<VolumeRange />} />
+      </Routes>
+    </Suspense>
   )
 }
 
